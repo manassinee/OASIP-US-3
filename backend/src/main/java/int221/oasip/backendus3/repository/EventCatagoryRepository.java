24029124cc932b0dde698a74e2edb57902b171c0
Rooted <modhanami@gmail.com>
2022-05-02 12:15:19 +0700

    Change domain type for EventCatagoryRepository

--- backend/src/main/java/int221/oasip/backendus3/repository/EventCatagoryRepository.java
+++ backend/src/main/java/int221/oasip/backendus3/repository/EventCatagoryRepository.java
@@ -1,7 +1,7 @@
 package int221.oasip.backendus3.repository;
 
-import int221.oasip.backendus3.entities.Event;
+import int221.oasip.backendus3.entities.EventCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventCatagoryRepository extends JpaRepository<Event, Integer> {
+public interface EventCatagoryRepository extends JpaRepository<EventCategory, Integer> {
 }
