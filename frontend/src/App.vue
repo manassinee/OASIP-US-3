bc35e1d7f2368108b71abf92fa11a79499030069
Rooted <modhanami@gmail.com>
2022-05-07 19:58:49 +0700

    Add base navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,9 +2,18 @@
 </script>
  
 <template>
-<router-view></router-view>
+  <nav class="bg-white px-10 py-4 flex">
+    <div class="flex gap-6 text-sm font-medium">
+      <router-link to="/" class="text-gray-700 hover:text-pink-600">Home</router-link>
+      <router-link :to="{ name: 'createEvent' }" class="text-gray-700 hover:text-pink-600">Create Event</router-link>
+    </div>
+  </nav>
+
+  <router-view></router-view>
 </template>
  
 <style scoped>
-
+.router-link-active {
+  @apply text-pink-600;
+}
 </style>
