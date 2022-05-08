6138c7bb45820c0f227ca04319b91e9547456b38
manassinee <manassinee.v@mail.kmutt.ac.th>
2022-05-08 22:10:22 +0700

    [add] createEvent function

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,14 +1,41 @@
-const baseUrl = import.meta.env.PROD ? import.meta.env.VITE_API_URL : '/api';
+const baseUrl = import.meta.env.PROD ? import.meta.env.VITE_API_URL : "/api";
 
 function makeUrl(path) {
   return `${baseUrl}${path}`;
 }
 
 export async function getEvents() {
-  const response = await fetch(makeUrl('/events'));
+  const response = await fetch(makeUrl("/events"));
   if (response.status === 200) {
-    return response.json()
+    const events = response.json();
+    return events;
   } else {
-    console.log('Cannot fetch events');
+    console.log("Cannot fetch events");
+  }
+}
+
+export async function getCategories() {
+  const response = await fetch(makeUrl("/categories"));
+  if (response.status === 200) {
+    const categories = response.json();
+    return categories;
+  } else {
+    console.log("Cannot fetch events");
+  }
+}
+
+export async function createEvent(newEvent) {
+  const response = await fetch(makeUrl("/events"), {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newEvent),
+  });
+  if (response.status === 201) {
+    const addedEvent = await response.json();
+    return addedEvent;
+  } else {
+    console.log("Cannot create event");
   }
 }
--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -3,3 +3,14 @@ export function sortDescendingByDateInPlace(arr, keyExtractor) {
     return new Date(keyExtractor(b)).getTime() - new Date(keyExtractor(a)).getTime();
   });
 }
+
+export function formatDateTimeLocal(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  const formatted = `${year}-${month < 10 ? '0' : ''}${month}-${day < 10 ? '0' : ''}${day}T${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+  return formatted;
+}
