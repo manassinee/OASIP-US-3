f2f7d9244c8a12900951979c7af6eb683c20cc95
Rooted <modhanami@gmail.com>
2022-05-09 19:55:57 +0700

    Replace event popup with side pane

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -14,3 +14,18 @@ export function formatDateTimeLocal(date) {
   const formatted = `${year}-${month < 10 ? '0' : ''}${month}-${day < 10 ? '0' : ''}${day}T${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
   return formatted;
 }
+
+export function formatDateAndFromToTime(date, durationMinute) {
+  const from = new Date(date);
+  const to = new Date(date);
+  to.setMinutes(to.getMinutes() + durationMinute);
+  return `${formatDate(from)} ${formatTime(from)} - ${formatTime(to)}`;
+}
+
+export function formatTime(date) {
+  return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+}
+
+export function formatDate(date) {
+  return date.toLocaleDateString();
+}
--- frontend/src/views/Events.vue
+++ frontend/src/views/Events.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { onBeforeMount, ref } from "vue";
-import EventPopup from "../components/EventPopup.vue";
-import { getEvents, deleteEvent } from "../service/api";
+import EventDetails from "../components/EventDetails.vue";
+import { deleteEvent, getEvents } from "../service/api";
 import { sortDescendingByDateInPlace } from "../utils";
+import { formatDate, formatTime } from '../utils';
+
 
 const events = ref([]);
 
@@ -29,63 +31,78 @@ async function cancelEvent(event) {
   }
 
 }
+
+function formatDateTime(date) {
+  return `${formatDate(date)} ${formatTime(date)} `;
+}
 </script>
 
 <template>
-  <div class="p-10 max-w-[1024px] mx-auto">
-    <div class="mb-4 font-semibold">All Events: {{ events.length }} events</div>
-    <table
-      class="table-auto w-full text-md text-left text-gray-500 relative z-0"
-    >
-      <thead class="text-xs text-sky-500 uppercase bg-sky-50">
-        <tr class="text-sky-600">
-          <th class="px-6 py-3">Name</th>
-          <th class="px-6 py-3">Start Time</th>
-          <th class="px-6 py-3">Duration (mins)</th>
-          <th class="px-6 py-3">Category</th>
-          <th class="px-6 py-3">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-if="events.length > 0"
-          v-for="event in events"
-          @click="currentEvent = event"
-          class="my-10 blockbg-white rounded-lg shadow-md relative cursor-pointer hover:bg-gray-50 transition box-border"
-          :class="[
-            {
-              'z-10 bg-blue-200/10 hover:bg-blue-200/20 ring-2 ring-blue-400/50 ':
-                currentEvent.id === event.id
-            }
-          ]"
-        >
-          <td class="py-2 px-2">{{ event.bookingName }}</td>
-          <td class="py-2 px-2">
-            {{ new Date(event.eventStartTime).toLocaleString() }}
-          </td>
-          <td class="py-2 px-2">{{ event.eventDuration }}</td>
-          <td class="py-2 px-2">{{ event.eventCategory.eventCategoryName }}</td>
-          <td class="py-2 px-2">
-            <button class="relative text-red-400 z-50" @click="cancelEvent(event)">Delete</button>
-          </td>
-        </tr>
-        <tr v-else>
-          <td colspan="5" class="p-6 text-center">No Scheduled Event</td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="py-8 px-12 max-w-[1440px] flex mx-auto">
 
-    <EventPopup
-      v-if="currentEvent.id"
-      :name="currentEvent.bookingName"
-      :email="currentEvent.bookingEmail"
-      :startTime="currentEvent.eventStartTime"
-      :duration="currentEvent.eventDuration"
-      :category="currentEvent.eventCategory.eventCategoryName"
-      :notes="currentEvent.eventNotes"
-      @close="currentEvent = {}"
-    />
+    <div class="flex flex-col">
+      <div class="mb-4 font-semibold">All Events: {{ events.length }} events</div>
+      <div class="flex">
+        <table class="table-fixed text-md text-left w-8/12 flex-1">
+
+          <thead class="text-xs text-sky-500 uppercase bg-sky-50">
+            <tr class="text-sky-600">
+              <th class="px-6 py-3">Name</th>
+              <th class="px-6 py-3">Schedule</th>
+              <th class="px-6 py-3">Category</th>
+              <th class="px-6 py-3">Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr v-if="events.length > 0" v-for="event in events" @click="currentEvent = event"
+              class="my-10 blockbg-white rounded-lg shadow-md relative cursor-pointer hover:bg-gray-50 transition box-border"
+              :class="[
+                {
+                  'z-10 bg-blue-200/10 hover:bg-blue-200/20 ring-2 ring-blue-400/50 ':
+                    currentEvent.id === event.id,
+                }
+              ]">
+
+              <td class="py-2 px-2">
+                <span class="font-medium text-gray-800">{{ event.bookingName }}</span>
+              </td>
+
+              <td class="py-2 px-2">
+                <div class="flex flex-col">
+                  <span class="">{{ formatDateTime(new Date(event.eventStartTime)) }}</span>
+                  <span class="text-sm text-gray-500">{{ event.eventDuration }} minutes</span>
+                </div>
+              </td>
+
+              <td class="py-2 px-2">
+                <div class="flex">
+                  <div class="text-sm text-sky-600 font-semibold rounded-lg bg-sky-50 px-3">
+                    {{ event.eventCategory.eventCategoryName }}
+                  </div>
+                </div>
+              </td>
+
+              <td class="py-2 px-2">
+                <button class="text-red-400 z-50" @click.stop="cancelEvent(event)">Delete</button>
+              </td>
+
+            </tr>
+            <tr v-else>
+              <td colspan="5" class="p-6 text-center">No Scheduled Event</td>
+            </tr>
+          </tbody>
+
+        </table>
+
+        <div class="p-4 bg-gray-100 relative w-4/12" v-if="currentEvent.id">
+          <EventDetails class="sticky top-24" :currentEvent="currentEvent" @close="currentEvent = {}" />
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
