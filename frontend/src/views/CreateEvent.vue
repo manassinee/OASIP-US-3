3f427441ead01f461166c0989ef5ba6e9403489b
Rooted <modhanami@gmail.com>
2022-05-10 13:11:50 +0700

    Refactor CreateEvent input values

--- frontend/src/views/CreateEvent.vue
+++ frontend/src/views/CreateEvent.vue
@@ -3,13 +3,19 @@ import { onBeforeMount, ref, watch } from "vue";
 import { createEvent, getCategories } from "../service/api";
 import { formatDateTimeLocal } from "../utils";
 
-const bookingName = ref('');
-const bookingEmail = ref('')
-const eventStartTime = ref('')
-const eventCategory = ref('');
-const eventNotes = ref('');
-
+// init inputs with default values
+const inputs = ref(makeDefaultValues());
 const categories = ref([]);
+
+function makeDefaultValues() {
+  return {
+    bookingName: '',
+    bookingEmail: '',
+    eventStartTime: '',
+    eventCategoryId: '',
+    eventNotes: ''
+  };
+}
 
 onBeforeMount(async () => {
   categories.value = await getCategories();
@@ -20,59 +26,50 @@ const minDateTImeLocal = formatDateTimeLocal(new Date());
 
 async function handleSubmit() {
   const event = {
-    bookingName: bookingName.value,
-    bookingEmail: bookingEmail.value,
+    ...inputs.value,
 
-    // from 2022-02-02T02:02 to 2022-02-02T02:02:00Z (append the ':00Z')
-    eventStartTime: new Date(eventStartTime.value).toISOString(),
-
-    eventCategoryId: eventCategory.value.id,
-    eventNotes: eventNotes.value
-  }
+    // convert local time to UTC in ISO-8601 format
+    eventStartTime: new Date(inputs.value.eventStartTime).toISOString(),
+  };
 
   const createdEvent = await createEvent(event);
   if (createdEvent) {
-    bookingName.value = '';
-    bookingEmail.value = '';
-    eventStartTime.value = '';
-    eventCategory.value = '';
-    eventNotes.value = '';
+    // clear inputs
+    inputs.value = makeDefaultValues();
     alert('Successfully created the event');
   } else {
     alert('Sorry, something went wrong');
   }
 }
-
-watch(eventStartTime, (d) => {
-  console.log(d);
-});
 </script>
  
 <template>
-<div>
-<form @submit.prevent="handleSubmit" class="flex flex-col gap-4">
-  <label for="name">Booking Name </label>
-  <input id="name" type="text" v-model="bookingName" required class="bg-gray-100 p-2">
-  
-  <label for="email">Booking Email </label>
-  <input id="email" type="email" v-model="bookingEmail" required class="bg-gray-100 p-2">
-  
-  <label for="startTime">Event Start Time </label>
-  <input id="startTime" type="datetime-local" :min="minDateTImeLocal" v-model="eventStartTime" required class="bg-gray-100 p-2">
-      
-  <label for="category">Event Category </label>
-  <select v-model="eventCategory" required class="bg-gray-100 p-2">
-    <option v-for="category in categories" :value="category">{{ category.eventCategoryName }} - ({{ category.eventDuration }} minutes)</option>
-  </select>
+  <div>
+    <form @submit.prevent="handleSubmit" class="flex flex-col gap-4">
+      <label for="name">Booking Name </label>
+      <input id="name" type="text" v-model="inputs.bookingName" required class="bg-gray-100 p-2">
 
-  <label for="notes">Event Notes (optional)</label>
-  <textarea id="notes" v-model="eventNotes" class="bg-gray-100 p-2"></textarea>
-  
-  <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">submit</button>
-</form>
-</div>
+      <label for="email">Booking Email </label>
+      <input id="email" type="email" v-model="inputs.bookingEmail" required class="bg-gray-100 p-2">
+
+      <label for="startTime">Event Start Time </label>
+      <input id="startTime" type="datetime-local" :min="minDateTImeLocal" v-model="inputs.eventStartTime" required
+        class="bg-gray-100 p-2">
+
+      <label for="category">Event Category </label>
+      <select v-model="inputs.eventCategoryId" required class="bg-gray-100 p-2">
+        <option v-for="category in categories" :value="category.id">{{ category.eventCategoryName }} - ({{
+            category.eventDuration
+        }} minutes)</option>
+      </select>
+
+      <label for="notes">Event Notes (optional)</label>
+      <textarea id="notes" v-model="inputs.eventNotes" class="bg-gray-100 p-2"></textarea>
+
+      <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">submit</button>
+    </form>
+  </div>
 </template>
  
 <style scoped>
-
 </style>
