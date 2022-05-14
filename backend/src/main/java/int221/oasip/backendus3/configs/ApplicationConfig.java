4369b4058e37b5c27114f2f4151be6ce4b8b9504
manassinee <manassinee.v@mail.kmutt.ac.th>
2022-05-14 23:46:40 +0700

    [add] error handling

--- backend/src/main/java/int221/oasip/backendus3/configs/ApplicationConfig.java
+++ backend/src/main/java/int221/oasip/backendus3/configs/ApplicationConfig.java
@@ -1,13 +1,43 @@
 package int221.oasip.backendus3.configs;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
 
 @Configuration
 public class ApplicationConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public ErrorAttributes errorAttributes() {
+        return new DefaultErrorAttributes() {
+            @Override
+            public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
+                Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, options);
+                Throwable error = super.getError(webRequest);
+
+                if (error instanceof MethodArgumentNotValidException) {
+                    MethodArgumentNotValidException exception = (MethodArgumentNotValidException) error;
+                    FieldError fieldError = exception.getFieldError();
+
+                    if (fieldError != null) {
+                        errorAttributes.put("message", fieldError.getDefaultMessage());
+                    }
+                }
+
+                return errorAttributes;
+            }
+        };
     }
 }
