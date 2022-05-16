1413b715d656e10a684d96949f25e651fa2e9786
Rooted <modhanami@gmail.com>
2022-05-16 20:43:46 +0700

    Change backend validation error response

--- backend/src/main/java/int221/oasip/backendus3/configs/ApplicationConfig.java
+++ backend/src/main/java/int221/oasip/backendus3/configs/ApplicationConfig.java
@@ -10,6 +10,9 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Configuration
@@ -29,10 +32,17 @@ public class ApplicationConfig {
 
                 if (error instanceof MethodArgumentNotValidException) {
                     MethodArgumentNotValidException exception = (MethodArgumentNotValidException) error;
-                    FieldError fieldError = exception.getFieldError();
 
-                    if (fieldError != null) {
-                        errorAttributes.put("message", fieldError.getDefaultMessage());
+                    if (exception.hasFieldErrors()) {
+                        // make a map of field errors. keyed by field name, with value of an array of error messages for that field
+                        Map<String, List<String>> map = new HashMap<>();
+
+                        for (FieldError fieldError : exception.getFieldErrors()) {
+                            map.computeIfAbsent(fieldError.getField(), k -> new ArrayList<>()).add(fieldError.getDefaultMessage());
+                        }
+
+                        errorAttributes.put("message", "Validation failed");
+                        errorAttributes.put("errors", map);
                     }
                 }
 
