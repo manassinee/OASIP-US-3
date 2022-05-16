c88d122877367b2d6d10577a4b184d1fa8aeba59
Rooted <modhanami@gmail.com>
2022-05-17 02:55:51 +0700

    Add validation error response + overlap validation for backend

--- backend/src/main/java/int221/oasip/backendus3/configs/ApplicationConfig.java
+++ backend/src/main/java/int221/oasip/backendus3/configs/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package int221.oasip.backendus3.configs;
 
+import int221.oasip.backendus3.controllers.OverlapEventException;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
@@ -10,6 +11,9 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Configuration
@@ -26,14 +30,28 @@ public class ApplicationConfig {
             public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
                 Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, options);
                 Throwable error = super.getError(webRequest);
+                Map<String, List<String>> errorMaps = new HashMap<>();
 
                 if (error instanceof MethodArgumentNotValidException) {
                     MethodArgumentNotValidException exception = (MethodArgumentNotValidException) error;
-                    FieldError fieldError = exception.getFieldError();
 
-                    if (fieldError != null) {
-                        errorAttributes.put("message", fieldError.getDefaultMessage());
+                    if (exception.hasFieldErrors()) {
+                        // make a errorMaps of field errors. keyed by field name, with value of an array of error messages for that field
+
+                        for (FieldError fieldError : exception.getFieldErrors()) {
+                            errorMaps.computeIfAbsent(fieldError.getField(), k -> new ArrayList<>()).add(fieldError.getDefaultMessage());
+                        }
+
+                        errorAttributes.put("message", "Validation failed");
+                        errorAttributes.put("errors", errorMaps);
                     }
+                }
+
+                if (error instanceof OverlapEventException) {
+                    OverlapEventException exception = (OverlapEventException) error;
+                    errorMaps.put(exception.getField(), List.of(exception.getMessage()));
+                    errorAttributes.put("message", "Validation failed");
+                    errorAttributes.put("errors", errorMaps);
                 }
 
                 return errorAttributes;
--- backend/src/main/java/int221/oasip/backendus3/repository/EventRepository.java
+++ backend/src/main/java/int221/oasip/backendus3/repository/EventRepository.java
@@ -2,6 +2,24 @@ package int221.oasip.backendus3.repository;
 
 import int221.oasip.backendus3.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
+    // get all overlap events. there are two scenarios:
+    // 1. events that started before the startTime and ended after the startTime
+    // 2. events that started between the startTime (inclusive) and the endTime (exclusive)
+    @Query(nativeQuery = true,
+            value = "SELECT * FROM Event e WHERE " +
+                    "e.eventCategoryId = ?3 AND " +
+                    "((e.eventStartTime < ?1 AND (e.eventStartTime + INTERVAL e.eventDuration MINUTE) > ?1) OR " +
+                    "(e.eventStartTime >= ?1 AND e.eventStartTime < ?2))")
+    List<Event> findOverlapEventsByCategoryId(Instant startTime, Instant endTime, Integer categoryId);
+
+    @Query(nativeQuery = true,
+            value = "SELECT * FROM Event e WHERE e.eventCategoryId = ?1 AND DATE(e.eventStartTime) = ?2")
+    List<Event> findByCategoryIdOnDate(Integer categoryId, LocalDate date);
 }
