00c3689c5c882616efe5510330c89d8cb4fc16db
Thanawat Naeching <thanawat.jack@mail.kmutt.ac.th>
2022-05-23 11:37:55 +0700

    1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,7 @@
     <div class="flex gap-6 text-sm font-medium">
       <router-link to="/" class="text-gray-700 hover:text-sky-600">Home</router-link>
       <router-link :to="{ name: 'createEvent' }" class="text-gray-700 hover:text-sky-600">Create Event</router-link>
+      <router-link :to="{ name: 'categoryEvent' }" class="text-gray-700 hover:text-sky-600">Category Event</router-link>
     </div>
   </nav>
 
